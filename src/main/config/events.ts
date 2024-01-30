dd055c47afe7332ddcc9c5548c5538e73e834bbc
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-30 16:38:55 -0300

    fix: logic breaked

--- src/main/config/events.ts
+++ src/main/config/events.ts
@@ -1,34 +1,37 @@
 import type { Server, Socket } from "socket.io";
-import { socketClient } from "./socket-client";
 
 export const makeEvents = (server: Server) => {
   server.on("connection", async (socket: Socket) => {
+    console.log(socket.id);
+
     socket.on("banLog", async (data: any) => {
-      socketClient.emit("banLog", data);
+      server.emit("banLog", data);
     });
 
     socket.on("chatLog", async (data: any) => {
-      socketClient.emit("chatLog", data);
+      console.log(data);
+
+      server.emit("chat", data);
     });
 
     socket.on("gameLog", async (data: any) => {
-      socketClient.emit("gameLog", data);
+      server.emit("gameLog", data);
     });
 
     socket.on("newPlayerProfileLog", async (data: any) => {
-      socketClient.emit("newPlayerProfileLog", data);
+      server.emit("newPlayerProfileLog", data);
     });
 
     socket.on("reportLog", async (data: any) => {
-      socketClient.emit("reportLog", data);
+      server.emit("reportLog", data);
     });
 
     socket.on("adminLog", async (data: any) => {
-      socketClient.emit("adminLog", data);
+      server.emit("adminLog", data);
     });
 
     socket.on("ticketsLog", async (data: any) => {
-      socketClient.emit("ticketsLog", data);
+      server.emit("ticketsLog", data);
     });
   });
 };
