fcbe10720c77924c876af6ef748086d01dcb0f8d
willian <willian.cacoelho@gmail.com>
2024-02-06 00:11:07 -0300

    test: ensure KillLogEvent calls emit with correct values

---
+++ tests/presentation/events/kill-log.spec.ts
@@ -0,0 +1,34 @@
+import { server } from "../../../src/main/config/app";
+import { KillLogEvent } from "../../../src/presentation/events/kill-log";
+
+jest.mock("../../../src/main/config/app", () => ({
+  server: {
+    emit: jest.fn(),
+  },
+}));
+
+const makeSut = () => {
+  const sut = new KillLogEvent();
+
+  return {
+    sut,
+  };
+};
+
+const fakeData = {
+  id: "fake-id",
+};
+
+describe("GameStateLog Event", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should call emit with correct values", async () => {
+    const { sut } = makeSut();
+
+    await sut.handle(fakeData);
+
+    expect(server.emit).toHaveBeenCalledWith("kill", fakeData);
+  });
+});
