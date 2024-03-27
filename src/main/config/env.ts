2b450a705d27f3777e1062c5d87b50744acbf390
willian <willian.cacoelho@gmail.com>
2024-03-27 14:54:12 -0300

    refactor: remove bot url and client

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,6 @@ import { config } from "dotenv";
 config();
 
 export const env = {
-  apiUrl: process.env.BOT_URL || "http://localhost:7070",
   port: process.env.PORT || 8080,
   key: process.env.KEY,
   cert: process.env.CERT,
--- src/main/config/socket-client.ts
+++
@@ -1,4 +0,0 @@
-import { io } from "socket.io-client";
-import { env } from "./env";
-
-export const socketClient = io(env.apiUrl);
